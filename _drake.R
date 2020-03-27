19b80ff2c191856ba727fde3056d78224534c62f
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2020-03-27 09:20:18 -0400

    updates to strocture (to use r_make())

--- _drake.R
+++ _drake.R
@@ -2,10 +2,8 @@ source("R/packages.R")  # loads packages
 source("R/functions.R") # defines the create_plot() function
 source("R/plan.R")      # creates the drake plan
 
-outdated(plan)
-
-make(plan)
-
 vis_drake_graph(plan)
+
+drake_config(plan, verbose = 2)
 
 # drake::clean()
